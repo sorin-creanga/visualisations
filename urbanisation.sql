24927035d4a2c567344d7e167b60d8d7f264e0e4
sorin.creanga <screan01@student.bbk.ac.uk>
2025-12-26 17:43:35 +0000

    Updated SQL Query

--- urbanisation.sql
+++ urbanisation.sql
@@ -1,18 +1,23 @@
+CREATE VIEW world.urbanisation AS
+
 SELECT 
-    c.Code,
-    c.Name,
-    c.Continent,
-    c.Population,
+    c.code, 
+    c.name AS CountryName, 
+    c.IndepYear, 
+    c.Population AS CountryPopulation, 
     c.GNP,
-    round((c.GNP / c.Population),3) AS gnp_per_capita,
-    (c.GNP - COALESCE(c.GNPOld, c.GNP)) AS gnp_delta,
+    COALESCE(c.GNPOld, 0) AS GNPOld,
+    ROUND((c.GNP / c.Population), 3) AS gnp_per_capita,
+    (c.GNP - COALESCE(c.GNPOld, 0)) AS gnp_delta, 
+    city.Name AS CapitalName,
+    city.Population AS CapitalPopulation,
     (SELECT SUM(Population) FROM City WHERE CountryCode = c.Code) / c.Population AS urbanization_rate,
     CASE 
         WHEN c.LifeExpectancy > (SELECT AVG(LifeExpectancy) FROM Country WHERE LifeExpectancy IS NOT NULL) THEN 1 
         ELSE 0 
     END AS target_high_life
-FROM Country c
-WHERE 
-  c.LifeExpectancy IS NOT NULL 
-  AND c.Population >= 1000000 
-AND Round((c.GNP / c.Population),3) > 0; -- gnp_per_capita higher than 0
+FROM country AS c
+LEFT JOIN city ON c.Capital = city.ID
+WHERE c.IndepYear IS NOT NULL
+  AND c.Population > 1000000
+  AND ROUND((c.GNP / c.Population), 3) > 0;
