14a16a2a9130f31100f0b898cecb083d955b69b9
sorin.creanga <screan01@student.bbk.ac.uk>
2026-01-12 20:06:12 +0000

    Started working on Streamlit dashboard

---
+++ dashboard_streamlit.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import streamlit as st
+
+# page configs
+
+st.set_page_config(
+    page_title= "World Dateset Learnings",
+    
+    initial_sidebar_state="expanded",
+    layout = "wide",
+    
+)
+
+
+
+st.title("🌎 World Data Learning")
+st.markdown("Population & Language")
+
+
